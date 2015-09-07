1613df0a80b140395905026c2fac09f8fdf7835a
Manu <bionikspoon@gmail.com>
2015-09-06 23:28:33 -0500

    PyCharm 4.5.4 <manu@msp Create editor.xml, databaseDrivers.xml, Obsidian MSP.icls, find.xml, manu@msp.xml, vcs.xml, ide.general.xml, ui.lnf.xml, project.default.xml, Python Custom.xml, Obsidian.icls, keymap.xml, Default _1_.xml, JavaScript.xml, removedInterpreters.xml, colors.scheme.xml, intentionSettings.xml, diff.xml, Python Script.py, Darcula copy.icls, Deployment.xml, other.xml, Monokai MSP.icls, packages.xml, web-browsers.xml, applicationLibraries.xml, dbnavigator.xml, Default for KDE copy.xml, Default _2_.xml, jdk.table.xml, usageView.xml, terminal.xml, Python Unit Test.py, filetypes.xml, Setup Script.py, Django.xml, Javascript Custom.xml, manu_msp.xml, npm_codestyle.xml, debugger.xml, Darcula MSP.icls, Flask Main.py, baseRefactoring.xml, emmet.xml, github_settings.xml, git.xml, scratches.xml, uml.xml, laf.xml, editor.codeinsight.xml, ignore.xml, markdown.xml, Default.xml, Django Custom.xml, code.style.schemes.xml

---
+++ fileTemplates/internal/Setup Script.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding=utf-8
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='$Package_name',
+    version='$Version',
+    packages=$PackageList,$PackageDirs
+    url='$URL',
+    license='$License',
+    author='$Author',
+    author_email='$Author_Email',
+    description='$Description'
+)
